1e9d7eac82d86370b99437eb434ac726eccaea5c
weifengsmile <516202795@qq.com>
2017-03-15 23:46:01 +0800

    feat(frontend): add the I label edit

--- client/src/components/write/Write.js
+++ client/src/components/write/Write.js
@@ -21,9 +21,44 @@ class Write extends Component {
     }
 
     changeContent = (e) => {
+        let string = this.refs.content.innerHTML
         this.setState({
             content: this.refs.content.innerHTML
         })
+    }
+
+    toggleI = (e) => {
+        e.stopPropagation()
+        if (window.getSelection) {
+            let selecter = window.getSelection()
+            let selectStr = selecter.toString()
+            if (selectStr.trim != "") {
+                // var rang = selecter.getRangeAt(0)
+                if (selecter.focusNode.parentNode.tagName == 'I') {
+                    // var docFragment = rang.cloneContents()
+                    // var testDiv = document.createElement("div")
+                    // testDiv.appendChild(docFragment)
+                    // var selectHtml = testDiv.innerHTML.replace(/\<i\>/g,'').replace(/\<\/i\>/g,'')
+                    // window.getSelection().createRange().pasteHTML('<i>' + selectStr + '</i>')
+                    document.execCommand('InsertHtml','', selectStr)
+                } else {
+                    document.execCommand('InsertHtml','', '<i>' + selectStr + '</i>')
+                    // var rang = selecter.getRangeAt(0)
+                    // var temp = $('<i>' + selectStr + '</i>')
+                    // rang.surroundContents(temp[0])
+                }
+
+            }
+        } else if (document.selection) {
+            let selecter = document.selection.createRange()
+            selecter.select()
+            let selectStr = selecter.text//ie
+            if (document.selection.focusNode.parentNode.tagName == 'I') {
+                selecter.pasteHTML(selectStr)
+            } else {
+                selecter.pasteHTML('<i>' + selectStr + '</i>')
+            }
+        }
     }
 
     saveArticle = () => {
@@ -34,13 +69,14 @@ class Write extends Component {
         }
         writeActions.saveArticle(this.state.title, this.state.content)
     }
+
     render () {
         const {isActive, writeActions} = this.props
         return (
             <div className='text-center col-lg-8 col-lg-offset-2 writer-body'>
                 <div className="writer-options text-left">
                     <button className="btn btn-default option-btn">加粗</button>
-                    <button className="btn btn-default option-btn">倾斜</button>
+                    <button className="btn btn-default option-btn" onClick={this.toggleI}>倾斜</button>
                     <button className="btn btn-default option-btn">h1</button>
                     <button className="btn btn-warning submit-btn" onClick={this.saveArticle}>保存</button>
                 </div>
